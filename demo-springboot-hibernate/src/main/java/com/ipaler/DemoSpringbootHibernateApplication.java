c172414cba86b98230174dcbd8ca3efa21260135
Major Wang <ipaler@163.com>
2017-10-16 16:55:38 +0800

    Initial commit

---
+++ demo-springboot-hibernate/src/main/java/com/ipaler/DemoSpringbootHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.ipaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootHibernateApplication.class, args);
+	}
+}
---
+++ demo-springboot-mybatis/src/test/java/com/ipaler/DemoSpringbootMybatisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ipaler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringbootMybatisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
